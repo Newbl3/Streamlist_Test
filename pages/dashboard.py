fd6cf2c7b511d81dd2455d17d6d3ba04ef3c19bd
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 08:17:51 +0900

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -9,8 +9,28 @@ from sklearn import datasets
 #import matplotlib
 
 dataset_name = st.sidebar.selectbox("select Dataset", ("Iris", "Breast Cancer", "Wine dataset"))
-
 classifier_name= st.sidebar.selectbox("select Classifier", ("KNN", "SVM", "RandomForest"))
+
+def get_dataset(dataset_name):
+    if dataset_name =="Iris":
+        data = dataset.load_iris()
+    elif dataset_name ="Breast Cancer":
+        data = datasets.load_breast_cancer()
+    else:
+        data = datasets.load_wine()
+    X = data.data
+    y = data.target
+    return X,y
+X,y = get_dataset(dataset_name)
+st.write("shape of dataset", X.shape)
+st.write("number of classes", len(np.unique(y)))
+
+def add_parameter_ui(clf_name):
+    params = dict()
+    if clf_name =="KNN":
+        K = st.sidebar.slider("K",1,15)
+        params["K"] = K
+    return params
 
 
 # フォルダ階層に基づいてモデルファイルへの絶対パスを取得
