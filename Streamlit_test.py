595916df13f56e4d030662902c1f62a870c9e248
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 01:43:22 +0900

    Update Streamlit_test.py

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -7,7 +7,7 @@ st.write("これはとても簡単な Streamlit アプリです！")
 file_buffer = st.file_uploader("ファイルをアップロードしてください")
 
 # Excelファイルの読み込み
-df = pd.read_excel("data.xlsx")
+df = pd.read_excel("Data1.xlsx")
 
 # 表を表示
 st.write("データ", df)
