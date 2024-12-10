a5041b9600cf65244c50cf7c839b3e536dd55cc3
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 05:21:01 +0900

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from services import calculate, file_service
 
 def display():
@@ -27,10 +26,40 @@ def display():
         # グラフプロット
         st.line_chart(data=calc_df, x=x_column, y=y_column)
 
+def classify_uploaded_csv(uploaded_files, model, scaler, imputer):
+    #モデルとスケーラーの読み込み
+    model = pickle.load(open(f'/model/svm_model_rbf_best.pkl', 'rb'))
+    scaler = pickle.load(open(f'/model/scaler.pkl', 'rb'))
+    imputer = pickle.load(open(f'/model/imputer.pkl', 'rb'))
 
-        #モデルとスケーラーの読み込み
-        model = pickle.load(open(f'/pkl/svm_model_rbf_best.pkl', 'rb'))
-        scaler = pickle.load(open(f'/pkl/scaler.pkl', 'rb'))
-        imputer = pickle.load(open(f'/pkl/imputer.pkl', 'rb'))
-        pred = classify_movement_in_folder(training_folder, svm_model, scaler, imputer, labels)
-        st.write("動作分類結果",:pred)
+    # Load new data from uploaded file
+    string_data = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    new_data = pd.read_csv(string_data)
+
+    # Remove 'time' column if it exists
+    if 'time' in new_data.columns:
+        new_data = new_data.drop(columns=['time'])
+        # Remove first and last 30 frames
+        new_data = new_data.iloc[60:-60].reset_index(drop=True)
+        # Create moving window data
+        window_size = 30
+        X_new = [new_data.iloc[i:i + window_size].values.flatten()
+                 for i in range(0, len(new_data) - window_size + 1)]
+        X_new = np.array(X_new)
+        # Handle missing values
+        X_new = imputer.transform(X_new)
+        # Standardize the data
+        X_new = scaler.transform(X_new)
+        # Make predictions for each frame
+        predictions = model.predict(X_new)
+        # Count occurrences of each label and decide final prediction using majority vote
+        unique, counts = np.unique(predictions, return_counts=True)
+        final_prediction_counts = dict(zip(unique, counts))
+        final_prediction = unique[np.argmax(counts)].lower()  # Convert to lowercase
+        predicted_labels.append(final_prediction)
+        # Display results for the file
+        st.write(f"File: {file_name} | True Label: {true_label} | Label Counts: {final_prediction_counts} | Final Prediction: {final_prediction}")
+
+        
+    pred = classify_movement_in_folder(training_folder, svm_model, scaler, imputer, labels)
+    st.write("動作分類結果",:pred)
