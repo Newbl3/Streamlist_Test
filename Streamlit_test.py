7d826c0a67f515d32a8dd84096e51219d897e85c
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 01:52:09 +0900

    Update Streamlit_test.py

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -4,14 +4,8 @@ import streamlit as st
 # タイトル
 st.title("売上データの表示")
 
-# ファイル読み込みのエラーハンドリング
-try:
-    # Excelファイルの読み込み
-    df = pd.read_excel("Data1.xlsx")
-    # 表を表示
-    st.write("データ", df)
-except FileNotFoundError:
-    st.error("エラー: 指定されたファイル 'Data1.xlsx' が見つかりませんでした。")
-except Exception as e:
-    st.error(f"エラー: ファイルを読み込む際に問題が発生しました。詳細: {e}")
+# Excelファイルの読み込み
+df = pd.read_excel("Data1.xlsx")
 
+# 表を表示
+st.write("データ", df)
