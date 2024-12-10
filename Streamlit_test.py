81ff55e51a5e469c3219e63f7d7c9028d9704d84
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 01:39:32 +0900

    Update Streamlit_test.py

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.write("hello streamlit")
+st.title("Hello, Streamlit!")
+st.write("これはとても簡単な Streamlit アプリです！")
+
+# ↓以下を追加
+file_buffer = st.file_uploader("ファイルをアップロードしてください")
