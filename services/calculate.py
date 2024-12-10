2a50dc6c60f96aa5bf262fb129ed111f3888ea2b
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 03:43:33 +0900

    Update calculate.py

--- services/calculate.py
+++ services/calculate.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 def calculate(df: pd.DataFrame, pelvis: str, pelvis_list: str) -> pd.DataFrame:
     calc_df = df.copy()
-    calc_df['sales_with_tax'] = calc_df[pelvis] * calc_df[pelvis_list]
+    calc_df['total'] = calc_df[pelvis] * calc_df[pelvis_list]
     return calc_df
