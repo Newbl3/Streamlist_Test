a1110bef842768b1c47dab7fae4ede404a4cc58d
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 06:47:54 +0900

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from io import StringIO
 import pickle
-from sklearn import svm
+from scikit-learn import svm
 
 # フォルダ階層に基づいてモデルファイルへの絶対パスを取得
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  # pagesフォルダの親ディレクトリ
