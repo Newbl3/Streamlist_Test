ddd63e68a9644a6c3dbe9c00fbeed872883fde27
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 03:48:05 +0900

    Update calculate.py

--- services/calculate.py
+++ services/calculate.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def calculate(df: pd.DataFrame, pelvis: str, pelvis_list: str) -> pd.DataFrame:
+def calculate(df: pd.DataFrame, pelvis_tilt: str, pelvis_list: str) -> pd.DataFrame:
     calc_df = df.copy()
-    calc_df['total'] = calc_df[pelvis] * calc_df[pelvis_list]
+    calc_df['total'] = calc_df[pelvis_tilt] * calc_df[pelvis_list]
     return calc_df
