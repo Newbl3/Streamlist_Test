b4316239253cd618489b5815ecf48c9977105152
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 04:57:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ import streamlit as st
 from pages import dashboard
 
 def main():
-    st.title("CSV Data Processing and Graph Display")
-
-    model = pickle.load(open(f'{path}/pkl/clf.pkl', 'rb'))
-    pred = model.predict(datasets)
-    st.write("動作分類結果",:pred)
+    st.title("CSV Data Processing and Graph Display")    
     dashboard.display()
 
 if __name__ == "__main__":
