facb17a2cd12a2fd10e7e406dfc11e096ed79cde
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 04:50:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from pages import dashboard
 
 def main():
     st.title("CSV Data Processing and Graph Display")
+
+    model = pickle.load(open(f'{path}/pkl/clf.pkl', 'rb'))
+    pred = model.predict(datasets)
+    st.write("動作分類結果",:pred)
     dashboard.display()
 
 if __name__ == "__main__":
