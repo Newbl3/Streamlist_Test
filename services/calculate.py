08bf6f08d2db491ba197ba955c3c00ef96b0458d
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 03:19:14 +0900

    Update calculate.py

--- services/calculate.py
+++ services/calculate.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def calculate(df: pd.DataFrame, hip_flexion_r: str, hip_flexion_l: str) -> pd.DataFrame:
+def calculate(df: pd.DataFrame, pelvis_tilt: str, pelvis_list: str) -> pd.DataFrame:
     calc_df = df.copy()
-    calc_df['sales_with_tax'] = calc_df[hip_flexion_r] * calc_df[hip_flexion_l]
+    calc_df['sales_with_tax'] = calc_df[pelvis_tilt] * calc_df[pelvis_list]
     return calc_df
