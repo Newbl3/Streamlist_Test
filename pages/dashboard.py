f960bbe874e0ce62916cb5ad50c78ec8d0ceaf71
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 05:42:05 +0900

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -3,7 +3,7 @@ from services import calculate, file_service
 import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import seaborn as sns
 import streamlit as st
 from io import StringIO
