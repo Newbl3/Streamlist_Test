3e941133c13b053cf50912f7158eeaddfc2cd013
Newbl3 <159625126+Newbl3@users.noreply.github.com>
2024-12-11 17:08:05 +0900

    Update calculate.py

--- services/calculate.py
+++ services/calculate.py
@@ -1,12 +1,3 @@
-#import pandas as pd
-
-#def calculate(df: pd.DataFrame, pelvis_tilt: str, pelvis_list: str) -> pd.DataFrame:
-#    calc_df = df.copy()
-#    calc_df['total'] = calc_df[pelvis_tilt] * calc_df[pelvis_list]
-#    return calc_df
-
-
-#修正後
 import pandas as pd
 
 
